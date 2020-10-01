f77de92af656096771284ff138e4e79bbc254178
Wouter Deconinck <wdconinc@jlab.org>
2020-10-01 13:59:51 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget -q https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
 sudo dpkg -i cvmfs-release-latest_all.deb
 sudo apt-get -q update
@@ -6,9 +8,7 @@ rm -f cvmfs-release-latest_all.deb
 
 sudo mkdir -p /etc/cvmfs
 sudo mkdir -p /cvmfs/eic.opensciencegrid.org
+sudo service autofs stop
 sudo echo "CVMFS_REPOSITORIES=eic.opensciencegrid.org" | sudo tee /etc/cvmfs/default.local
-sudo /etc/init.d/autofs stop
-ls /cvmfs
-cat /etc/cvmfs/default.local
 sudo cvmfs_config setup
 sudo mount -t cvmfs eic.opensciencegrid.org /cvmfs/eic.opensciencegrid.org
